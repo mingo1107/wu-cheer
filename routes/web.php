3169443783275624a67e733f21fb58f8010d3898
mingo <os3310@gmail.com>
2025-11-18 15:32:15 +0800

    fix. router問題

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EarthDataController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// 登入頁面路由
-Route::get('/login', function () {
-    return view('cms-app');
-})->name('login');
-
 // 列印路由（需放在 SPA catch-all 前）
 Route::get('/print/earth-data/{id}/pending', [EarthDataController::class, 'printPending'])->name('print.earth-data.pending');
 Route::get('/print/earth-data/{id}/selected', [EarthDataController::class, 'printSelected'])->name('print.earth-data.selected');
 
 // SPA 路由 - 所有前端路由都指向同一個視圖
+// 包括根路由 /，這樣 Vue Router 才能正確處理所有路由
 Route::get('/{any}', function () {
     return view('cms-app');
 })->where('any', '.*');
